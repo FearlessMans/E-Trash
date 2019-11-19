be2a558847c3c52c7053ac850cd7c18faff8c601
FauzanAr <faurinta12345@gmail.com>
2019-11-19 18:46:04 +0700

    Removing unused code

--- routes/web.php
+++ routes/web.php
@@ -19,12 +19,7 @@ Auth::routes();
 Route::get('/admin', 'HomeController@index')->name('home')->middleware('auth');
 
 Route::group(['middleware' => 'auth'], function () {
-		Route::get('icons', ['as' => 'pages.icons', 'uses' => 'PageController@icons']);
-		Route::get('maps', ['as' => 'pages.maps', 'uses' => 'PageController@maps']);
-		Route::get('notifications', ['as' => 'pages.notifications', 'uses' => 'PageController@notifications']);
-		Route::get('rtl', ['as' => 'pages.rtl', 'uses' => 'PageController@rtl']);
 		Route::get('tables', ['as' => 'pages.tables', 'uses' => 'PageController@tables']);
-		Route::get('typography', ['as' => 'pages.typography', 'uses' => 'PageController@typography']);
 });
 
 Route::group(['middleware' => 'auth'], function () {
