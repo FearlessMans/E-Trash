f3220a0500f1f5a0139578d077e6dcc5681dd77e
FauzanAr <faurinta12345@gmail.com>
2019-11-19 21:16:46 +0700

    Modify navbar

--- resources/views/layouts/navbars/navs/user.blade.php
+++ resources/views/layouts/navbars/navs/user.blade.php
@@ -1,16 +1,16 @@
 <nav>
     <ul class="nav justify-content-end">
         <li class="nav-item">
-            <a class="nav-link active" href="#">Active</a>
+            <a class="nav-link" href="#">Transaction Check</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" href="#">About</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
+            <a class="nav-link" href="#">Contact</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link " href="#">Disabled</a>
+            <a class="nav-link " href="#">Product</a>
         </li>
     </ul>
 </nav>
