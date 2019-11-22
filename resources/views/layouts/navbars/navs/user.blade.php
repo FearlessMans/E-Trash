cc6c8bab6bb994c37cd043fc50e72c50a6ada523
FauzanAr <faurinta12345@gmail.com>
2019-11-22 19:57:51 +0700

    Changing color of navbar

--- resources/views/layouts/navbars/navs/user.blade.php
+++ resources/views/layouts/navbars/navs/user.blade.php
@@ -1,13 +1,13 @@
 <nav>
     <ul class="nav justify-content-end">
         <li class="nav-item">
-            <a class="nav-link" href="#transaksi" style="color:burlywood">Transaction Check</a>
+            <a class="nav-link" href="#transaksi" style="color:olive">Transaction Check</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#product" style="color:burlywood">Product</a>
+            <a class="nav-link" href="#product" style="color:olive">Product</a>
         </li>
         <li class="nav-item">
-            <a class="nav-link" href="#about" style="color:burlywood">About</a>
+            <a class="nav-link" href="#about" style="color:olive">About</a>
         </li>
     </ul>
 </nav>
