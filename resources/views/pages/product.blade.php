70d10c8f862edba01e2b138f228c8973258aa815
FauzanAr <faurinta12345@gmail.com>
2019-11-23 15:40:42 +0700

    Creating table inside the product blade (50%)

--- resources/views/pages/product.blade.php
+++ resources/views/pages/product.blade.php
@@ -2,4 +2,33 @@
 
 @section('content')
 
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title">
+                    Product Table
+                </h4>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table tablesorter">
+                        <thead class="text-primary">
+                            <tr>
+                                <th>Nama Sampah</th>
+                                <th>Harga</th>
+                                <th>Jumlah</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 @endsection
