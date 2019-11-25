80fc885dfad22fbead05d68f34d4bc43e36febce
FauzanAr <faurinta12345@gmail.com>
2019-11-25 19:58:25 +0700

    Adding transaction form

--- resources/views/pages/userProduct.blade.php
+++ resources/views/pages/userProduct.blade.php
@@ -1,6 +1,17 @@
 @extends('layouts.userapp')
 @section('content')
 <div class="container">
-    {{$product->nama_sampah}}
+    <div class="text-center">{{$product->nama_sampah}}</div>
+    <form id="Form">
+        @csrf
+        @method('post')
+        <div class="form-group">
+            <label>Email Perusahaan</label>
+            <input type="text" class="email_pembeli" class="form-control" placeholder="Masukkan E-Mail">
+            <label>Jumlah {{$product->nama_sampah}}</label>
+            <input type="number" class="jumlah_sampah" class="form-control" placeholder="Masukkan Jumlah Sampah">
+        </div>
+        <button class="btn btn-fill btn-primary" id="submit">Checkout</button>
+    </form>
 </div>
 @endsection
