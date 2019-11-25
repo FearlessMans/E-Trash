b311656276de579668b9c7d0968a8525268adf35
FauzanAr <faurinta12345@gmail.com>
2019-11-25 20:15:34 +0700

    fixing transaction form layout

--- resources/views/pages/userProduct.blade.php
+++ resources/views/pages/userProduct.blade.php
@@ -2,16 +2,19 @@
 @section('content')
 <div class="container">
     <div class="text-center">{{$product->nama_sampah}}</div>
-    <form id="Form">
+    <form>
         @csrf
         @method('post')
         <div class="form-group">
             <label>Email Perusahaan</label>
-            <input type="text" class="email_pembeli" class="form-control" placeholder="Masukkan E-Mail">
-            <label>Jumlah {{$product->nama_sampah}}</label>
-            <input type="number" class="jumlah_sampah" class="form-control" placeholder="Masukkan Jumlah Sampah">
+            <input type="email" class="form-control" name="email_pembeli" placeholder="Masukkan Email">
+            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
         </div>
-        <button class="btn btn-fill btn-primary" id="submit">Checkout</button>
+        <div class="form-group">
+            <label>Jumlah {{$product->nama_sampah}}</label>
+            <input type="number" class="form-control" name="jumlah_sampah" placeholder="Masukkan Jumlah Sampah">
+        </div>
+        <button type="submit" class="btn btn-primary" id="submit">Checkout</button>
     </form>
 </div>
 @endsection
